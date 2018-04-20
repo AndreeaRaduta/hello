b5b73e701299bf3855c3d097aae958aa3652b54b
AndreeaRaduta <andreear230@gmail.com>
2018-04-20 11:29:26 +0300

    Hello Person!

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 
 #include <stdio.h>
-int  main ()
+int  main (int argc, char *argv[])
+
 {
+if(argc != 2)
+{printf("USAGE: %s name\n, argv[0]);
+return -1;
+} 
 printf(" Hello ,  World !\n " ) ;
 return 0;
 }
